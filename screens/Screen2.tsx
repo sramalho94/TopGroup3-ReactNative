e2c2fa3e028a4569db6b427d98fb01b53c6bed48
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-18 18:31:22 -0400

    built out screen2

--- screens/Screen2.tsx
+++ screens/Screen2.tsx
@@ -1,19 +1,37 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
+import {RouteProp} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../App';
-
-type Props = {};
-
-export type NavigationProp = NativeStackNavigationProp<
+import {useNavigation} from '@react-navigation/native';
+import {NavigationProp} from '@react-navigation/native';
+type Screen2NavigationProp = NativeStackNavigationProp<
   RootStackParamList,
   'Screen2'
 >;
 
-const Screen2 = (props: Props) => {
+type Screen2RouteProp = RouteProp<RootStackParamList, 'Screen2'>;
+
+type Props = {
+  navigation: Screen2NavigationProp;
+  route: Screen2RouteProp;
+};
+
+const Screen2 = ({route}: Props) => {
+  const navigation = useNavigation<NavigationProp>();
+  const {name, dark, opacity} = route.params;
   return (
-    <View>
-      <Text>Screen2</Text>
+    <View
+      className={`min-h-screen bg-${name} justify-center align-middle mx-auto `}>
+      <Text className={`text-${name}`}>Screen2</Text>
+      <Text>Name: {name}</Text>
+      <Text>Dark: {dark ? 'Yes' : 'No'}</Text>
+      <Text>Opacity: {opacity}</Text>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <View className="flex-row justify-center">
+          <Text>Go back</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
