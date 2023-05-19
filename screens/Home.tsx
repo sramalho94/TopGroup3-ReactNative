14d42559d33a71ad258dc36c4d110224a5e00b30
jeremyHarrell <hillvulf@gmail.com>
2023-05-19 09:28:25 -0500

    completed project without successful dark mode

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,8 +1,11 @@
-import {View, Text, SafeAreaView} from 'react-native';
+import { View, Text, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native';
 import React from 'react';
-import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import {RootStackParamList} from '../App';
-import {useNavigation} from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../App';
+import { useNavigation } from '@react-navigation/native';
+import ActionRow from '../components/ActionRow';
+import { useState } from 'react';
+import { useColorScheme } from 'nativewind';
 
 type Props = {};
 
@@ -13,13 +16,38 @@ export type NavigationProp = NativeStackNavigationProp<
 
 const Home = (props: Props) => {
   const navigation = useNavigation<NavigationProp>();
+  // const [theme, setTheme] = React.useState("")
+  const { colorScheme, setColorScheme } = useColorScheme()
+
+  // const toggleTheme = () => {
+  //   if (theme === 'light') {
+  //     setTheme('dark')
+  //   }
+  // }
+
   return (
     <SafeAreaView className="flex flex-row justify-center md:flex-col ">
-      <Text
-        className="text-customcolor"
-        onPress={() => navigation.navigate('Screen2')}>
-        Hello world
-      </Text>
+      <ScrollView>
+
+        <Text
+          className="text-customcolor font-bold bg-orange-300"
+        >
+          TOP App
+        </Text>
+
+        <TouchableOpacity onPress={() => { navigation.navigate('SubmittedScreen') }}
+          className='bg-purple-300'>
+          <ActionRow
+            title="Submit Test Result"
+            screen="Demo"
+            color="#7FFFD4"
+            vertical
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setColorScheme(colorScheme === 'light' ? "dark" : "light")}>
+          <Text>{`You are in ${colorScheme} mode.`}</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </SafeAreaView>
   );
 };
